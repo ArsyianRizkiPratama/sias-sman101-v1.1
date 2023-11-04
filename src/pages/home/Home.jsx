924fa4c69739614615649d02cbe88266117ae983
Arsyian Rizki Pratama <81981819+ArsyianRizkiPratama@users.noreply.github.com>
2023-11-04 19:45:20 +0700

    Menambah Pages & file Home

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import "./home.scss"
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h1 className="title">SMAN 101 Jakarta</h1>
+        </div>
+    )
+}
+
+export default Home
