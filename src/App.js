576a0c689192450782278abdbaafff13bb6fc6b0
Arsyian Rizki Pratama <81981819+ArsyianRizkiPratama@users.noreply.github.com>
2023-11-05 09:20:32 +0700

    Update App.js "React Router Dom & Menghubungkan setiap File"
    1. Menambahkan React Router Dom
    2. Menghubungkan setiap page ke dalah App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import Home from "./pages/home/Home";
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
+
 function App() {
   return (
     <div className="App">
-      SIAS SMAN 101 Jakarta
+    {/* React-Router Dom Start */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home/>} />
+          <Route path="login" element={<Login/>}/>
+          <Route path="products">
+            <Route index element={<List/>}/>
+            <Route path=":productId" element={<Single/>}/>
+            <Route path="new" element={<New/>}/>
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    {/* React Router Dom End */}
     </div>
   );
 }
